8b314b0c5b425cdae7e5c5378e5558601f7619e5
Amine Tahiri <atahiri@e2r10p2.1337.ma>
2021-01-23 12:59:27 +0100

    adding some changes to wordpress container there are some bugs starting mariadb

--- wordpress/config/sql.sh
+++ wordpress/config/sql.sh
@@ -1,23 +1,3 @@
-expect -c "
-    set timeout 10
-    spawn mysql -u root -p
-    expect \"Enter password:\"
-    send \"root\r\"
-    expect EOF
-"
-chown -R mysql: /var/lib/mysql
-service mysql start
-mysql -u root <<MYSQL_SCRIPT
-CREATE DATABASE wp_db;
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';
-FLUSH PRIVILEGES;
-MYSQL_SCRIPT
-
-expect -c "
-    set timeout 10
-    spawn mysql -u root -p
-    expect \"Enter password:\"
-    send \"toor\r\"
-    expect EOF
-"
+rc-status
+/etc/init.d/mariadb restart
+mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';FLUSH PRIVILEGES;"

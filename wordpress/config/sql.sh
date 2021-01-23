aa5e66dd517a6b33a9abc81e70149f5cf5c2264a
Amine Tahiri <atahiri@e2r10p2.1337.ma>
2021-01-23 16:16:14 +0100

    didn't show databases problem

--- wordpress/config/sql.sh
+++ wordpress/config/sql.sh
@@ -1,3 +1,3 @@
-rc-status
-/etc/init.d/mariadb restart
+#!/bin/bash
+/usr/bin/supervisord -c /etc/supervisord.conf -s > /dev/null
 mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';FLUSH PRIVILEGES;"

98ee95d4debdcf6af2e232611666c5ae9a3d1764
Amine Tahiri <atahiri@e2r10p2.1337.ma>
2021-01-25 09:53:34 +0100

    problem script sql.sh fixed

--- wordpress/config/sql.sh
+++ wordpress/config/sql.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-/usr/bin/supervisord -c /etc/supervisord.conf -s --nodaemon
 mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';FLUSH PRIVILEGES;"

1021f1784dfda1748b86f8ec13715bd0f27a61ae
Amine Tahiri <atahiri@e2r10p2.1337.ma>
2021-01-25 12:59:14 +0100

    import db to mysql

--- wordpress/config/sql.sh
+++ wordpress/config/sql.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';FLUSH PRIVILEGES;"
+mysql -u root wp_db < wp_db.sql

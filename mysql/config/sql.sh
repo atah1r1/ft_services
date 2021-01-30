dab1e7b8deab606d0442f36f4aa354f77bf4eaf4
Amine Tahiri <atahiri@e2r10p2.1337.ma>
2021-01-30 12:56:01 +0100

    mysql added still need to connect with wordpress

--- mysql/config/sql.sh
+++ mysql/config/sql.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+rc-status
+touch /run/openrc/softlevel
+/usr/bin/mysql_install_db
+rc-service mariadb start
 mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';FLUSH PRIVILEGES;"
 mysql -u root wp_db < wp_db.sql
+/usr/bin/mysqld_safe --datadir="/var/lib/mysql"

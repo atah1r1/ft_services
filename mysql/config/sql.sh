9c54df695b12855a093fe1c08cc72c8ee05442e8
Amine Tahiri <atahiri@e2r10p2.1337.ma>
2021-01-31 09:29:53 +0100

    exposing port 5050 to 80 in wordpress pod

--- mysql/config/sql.sh
+++ mysql/config/sql.sh
@@ -4,5 +4,5 @@ touch /run/openrc/softlevel
 /usr/bin/mysql_install_db
 rc-service mariadb start
 mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';FLUSH PRIVILEGES;"
-mysql -u root wp_db < wp_db.sql
+# mysql -u root wp_db < wp_db.sql
 /usr/bin/mysqld_safe --datadir="/var/lib/mysql"

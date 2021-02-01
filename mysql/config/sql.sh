f325a8446b1ad8afec96347d74409d6c16e31d60
Amine Tahiri <atahiri@e2r10p2.1337.ma>
2021-02-01 12:26:31 +0100

    wordpress and pma fixed

--- mysql/config/sql.sh
+++ mysql/config/sql.sh
@@ -3,6 +3,6 @@ rc-status
 touch /run/openrc/softlevel
 /usr/bin/mysql_install_db
 rc-service mariadb start
-mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';FLUSH PRIVILEGES;"
-# mysql -u root wp_db < wp_db.sql
+mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'%';FLUSH PRIVILEGES;"
+mysql -u root wp_db < wp_db.sql
 /usr/bin/mysqld_safe --datadir="/var/lib/mysql"

af43116a4ab26fed57bdfef551ca1c7502687b6e
Amine Tahiri <atahiri@e2r10p2.1337.ma>
2021-02-03 19:29:00 +0100

    fixed

--- commands.sh
+++ commands.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+
+minikube start --vm-driver virtualbox
+
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f yaml/ConfigMap.yaml
+
+
+
+docker build nginx -t nginx_service
+docker build wordpress -t wordpress_service
+docker build phpMyAdmin -t pma_service
+docker build mysql -t mysql_service
+
+kubectl apply -f yaml/nginx.yaml
+kubectl apply -f yaml/wordpress.yaml
+kubectl apply -f yaml/mysql.yaml
+kubectl apply -f yaml/phpmyadmin.yaml
+
 eval $(minikube docker-env)
 ssh-keygen -R 192.168.99.125 # to update certificate in MacOS to prevent confliction

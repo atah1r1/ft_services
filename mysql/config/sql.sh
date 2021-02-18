031b30d7a764864591188c02105945862c051adc
mac <atahiri@student.1337.ma>
2021-02-18 22:46:05 +0100

    mysql persisting data fixed

--- mysql/config/sql.sh
+++ mysql/config/sql.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-rc-status
-touch /run/openrc/softlevel
-mysql_install_db -u root --datadir=/var/lib/mysql
-rc-service mariadb start
+# rc-status
+# touch /run/openrc/softlevel
+/usr/bin/mysql_install_db --user=root --datadir="/var/lib/mysql"
+/usr/bin/mysqld_safe --datadir="/var/lib/mysql" > /dev/null &
+sleep 5
 mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'%';FLUSH PRIVILEGES;"
 mysql -u root wp_db < wp_db.sql
-# /usr/bin/mysqld_safe --datadir="/var/lib/mysql"
+/usr/bin/mysqld_safe --datadir="/var/lib/mysql"

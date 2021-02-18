099c38efebc8d172594168a1c653b4a74797fa53
Amine Tahiri <atahiri@student.1337.ma>
2021-02-18 12:40:22 +0100

    trying to fix mysql

--- mysql/config/sql.sh
+++ mysql/config/sql.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rc-status
 touch /run/openrc/softlevel
-/usr/bin/mysql_install_db
+mysql_install_db -u root --datadir=/var/lib/mysql
 rc-service mariadb start
 mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'%';FLUSH PRIVILEGES;"
 mysql -u root wp_db < wp_db.sql
-/usr/bin/mysqld_safe --datadir="/var/lib/mysql"
+# /usr/bin/mysqld_safe --datadir="/var/lib/mysql"

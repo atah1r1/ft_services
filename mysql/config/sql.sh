b8d16308e8c2b16821c229b358b4fca5fe26813e
Amine Tahiri <atahiri@student.1337.ma>
2021-02-19 10:22:22 +0100

    all about mysql fixed

--- mysql/config/sql.sh
+++ mysql/config/sql.sh
@@ -6,4 +6,5 @@
 sleep 5
 mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'%';FLUSH PRIVILEGES;"
 mysql -u root wp_db < wp_db.sql
+rm -rf wp_db.sql
 /usr/bin/mysqld_safe --datadir="/var/lib/mysql"

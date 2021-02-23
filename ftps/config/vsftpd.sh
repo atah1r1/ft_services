542ae17a6f6d1ae92a3d3576f15f8630bd3fb287
Amine Tahiri <atahiri@student.1337.ma>
2021-02-23 18:08:08 +0100

    vsftpd doesn't kill problem fixed

---
+++ ftps/config/vsftpd.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
--- ftps/livness.sh
+++ ftps/livness.sh
@@ -2,7 +2,7 @@
 for var in "$@"
 do
     result=$(supervisorctl status | grep "$var" | awk '{print $2}')
-    if [[ $result == "EXITED" ]]; then
+    if [[ $result == "EXITED" ]] || [[ $result == "STOPPED" ]]; then
         exit 1
     fi
 done

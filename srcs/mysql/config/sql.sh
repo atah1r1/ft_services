d621f4599db6367bf55a643f45c2ff6662c53d58
Amine Tahiri <atahiri@student.1337.ma>
2021-02-28 15:01:50 +0100

    I think it's FINISHED

--- srcs/mysql/config/sql.sh
+++ srcs/mysql/config/sql.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 /usr/bin/mysql_install_db --user=root --datadir="/var/lib/mysql"
-/usr/bin/mysqld_safe --datadir="/var/lib/mysql" > /dev/null &
+/usr/bin/mysqld --datadir="/var/lib/mysql" > /dev/null &
 sleep 5
 mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'%';FLUSH PRIVILEGES;"
 mysql -u root wp_db < wp_db.sql
-rm -rf wp_db.sql
-/usr/bin/mysqld_safe --datadir="/var/lib/mysql"
